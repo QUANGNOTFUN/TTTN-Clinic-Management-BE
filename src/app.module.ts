2bf79eed5c1b53fdb72a1768871780db5d96c0b6
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2025-08-26 00:46:19 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,2 +1,12 @@
-import { Module } from '@nestjs/common';import { AppController } from './app.controller';import { AppService } from './app.service';@Module({  imports: [],  controllers: [AppController],  providers: [AppService, PrismaService],})export class AppModule {}
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
