bef023587434b79f7eb4580a090cacb0b23009ef
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2025-09-19 18:33:09 +0700

    fix deploy

--- src/doctor-schedule/dto/update-doctor-schedule.dto.ts
+++ src/doctor-schedule/dto/update-doctor-schedule.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDoctorScheduleDto } from './create-doctor-schedule.dto';
-
-export class UpdateDoctorScheduleDto extends PartialType(CreateDoctorScheduleDto) {}
+import { PartialType } from '@nestjs/swagger';import { CreateDoctorScheduleDto } from './create-doctor-schedule.dto';export class UpdateDoctorScheduleDto extends PartialType(  CreateDoctorScheduleDto,) {}
